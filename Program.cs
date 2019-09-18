5f199e175aa45eaa3b6d3b45a5b90d9d82f1560e
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-18 08:20:52 -0400

    Fill out main method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace MyExtensions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Extension Methods *****\n");
+
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            // Use new integer functionality
+            Console.WriteLine("Value of myInt: {0}", myInt);
+            Console.WriteLine("Reversed digits of myInt: {0}", myInt.ReverseDigits());
+
+            Console.ReadLine();
         }
     }
 }
